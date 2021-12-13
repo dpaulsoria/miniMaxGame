ae34175c383c53ce425ade97a224afccfe65a898
JoseTorres2210 <jotomazz@espol.edu.ec>
2021-12-12 19:20:46 -0500

    Se agrega la función utilidad

--- src/main/java/espol/model/game/Game.java
+++ src/main/java/espol/model/game/Game.java
@@ -4,6 +4,7 @@ import espol.model.tda.BinaryTree;
 import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Game {
@@ -25,6 +26,53 @@ public class Game {
         Cell c = new Cell(new Pair(position.x, position.y), true);
         c.setImage((player.equals('X') ? board.getO():board.getX()));
         board.getGrid().add(c, position.x, position.y);
+    }
+    
+    public int utilidad(){
+        int pJugador=p(board.getMapa(),player);
+        int pBot=p(board.getMapa(),bot);        
+        return pJugador-pBot;
+    }
+    
+    public int p(TreeMap<Integer, espol.model.tda.ArrayList<Cell>> tablero, Character c){
+        int utilP=0;
+        int filas=0;
+        int columnas=0;
+        int diagonales=0;
+        espol.model.tda.ArrayList<Cell> fila0 = new espol.model.tda.ArrayList<Cell>();
+        espol.model.tda.ArrayList<Cell> fila1 = new espol.model.tda.ArrayList<Cell>();
+        espol.model.tda.ArrayList<Cell> fila2 = new espol.model.tda.ArrayList<Cell>();
+        fila0= tablero.get(0);
+        fila1= tablero.get(1);
+        fila2= tablero.get(2);
+        //columnas
+        for(int i=0;i<fila0.size();i++){
+            if((fila0.get(i).getC()==c || fila0.get(i).getC()=='n') && (fila1.get(i).getC()==c || fila1.get(i).getC()=='n') && (fila2.get(i).getC()==c || fila2.get(i).getC()=='n')){
+                columnas++;
+                }
+        }
+        //filas
+        int tmp=0;
+        for(Map.Entry<Integer, espol.model.tda.ArrayList<Cell>> par: tablero.entrySet()){  //comprobar si el caracter es el mismo o está vacío
+            tmp=0;                
+            espol.model.tda.ArrayList<Cell> array=par.getValue();
+                    for(int i=0;i<array.size();i++){
+                        if(array.get(i).getC()==c || array.get(i).getC()=='n')
+                            tmp++;                  
+                    } 
+            if(tmp==3){
+            filas++;
+            }
+        }
+        //diagonales 
+        if((fila0.get(0).getC()==c || fila0.get(0).getC()=='n') && (fila1.get(1).getC()==c || fila1.get(1).getC()=='n') && (fila2.get(2).getC()==c || fila2.get(2).getC()=='n')){
+            diagonales++;
+        }        
+        if((fila0.get(2).getC()==c || fila0.get(2).getC()=='n') && (fila1.get(1).getC()==c || fila1.get(1).getC()=='n') && (fila2.get(0).getC()==c || fila2.get(0).getC()=='n')){
+            diagonales++;
+        }        
+        utilP=filas+columnas+diagonales;                    
+        return utilP;
     }
 
     public Character getPlayer() { return player; }
